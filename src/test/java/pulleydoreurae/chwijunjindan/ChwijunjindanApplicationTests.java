97a55e35baa7454b162c0080279b3fd0a6d6c882
scv829 <scv829@naver.com>
2024-01-10 15:18:36 +0900

    Test: 회원가입 테스트 코드 추가

--- src/test/java/pulleydoreurae/chwijunjindan/ChwijunjindanApplicationTests.java
+++ src/test/java/pulleydoreurae/chwijunjindan/ChwijunjindanApplicationTests.java
@@ -1,11 +1,22 @@
 package pulleydoreurae.chwijunjindan;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import pulleydoreurae.chwijunjindan.User.UserService;
 
 @SpringBootTest
 class ChwijunjindanApplicationTests {
 
+	@Autowired
+	private UserService userService;
+	@Test
+	public void signUpTest(){
+		userService.createUser("test1", "1234", "준회원", "tester1@naver.com", "ROLE_ASSOCIATE");
+		userService.createUser("test2", "1234", "정회원", "tester2@naver.com", "ROLE_REGULAR");
+		userService.createUser("admin", "1234", "일반관리자", "admin@naver.com", "ROLE_ADMIN");
+	}
 	@Test
 	void contextLoads() {
 	}
