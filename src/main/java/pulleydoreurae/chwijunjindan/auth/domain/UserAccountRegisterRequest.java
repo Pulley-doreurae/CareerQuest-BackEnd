7dd585a2501133c5f291db63fedce83acc89ec46
pjh5365 <pjh5365@naver.com>
2024-01-12 16:43:26 +0900

    Feat: 회원가입 시 전화번호 추가
    회원가입 시 전화번호를 입력받을수 있도록 추가함
    
    Related to: #5

--- src/main/java/pulleydoreurae/chwijunjindan/auth/domain/UserAccountRegisterRequest.java
+++ src/main/java/pulleydoreurae/chwijunjindan/auth/domain/UserAccountRegisterRequest.java
@@ -2,6 +2,7 @@ package pulleydoreurae.chwijunjindan.auth.domain;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,6 +24,10 @@ public class UserAccountRegisterRequest {
 	@Email(message = "이메일 형식만 가능합니다.")    // 이메일 형식만 지정
 	@NotBlank(message = "이메일은 필수입니다.")
 	private String email;
+
+	@NotBlank
+	@Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "휴대폰 번호 형식에 맞춰 입력해주세요.")	// 전화번호 형식만 받기
+	private String phoneNum;
 
 	@NotBlank(message = "비밀번호는 필수입니다.")
 	@Size(min = 8, message = "비밀번호는 8자 이상으로 입력해주세요.")    // 비밀번호의 길이는 최소 8자
--- src/main/java/pulleydoreurae/chwijunjindan/auth/domain/UserAccountRegisterResponse.java
+++ src/main/java/pulleydoreurae/chwijunjindan/auth/domain/UserAccountRegisterResponse.java
@@ -14,5 +14,6 @@ public class UserAccountRegisterResponse {
 	private String userId;
 	private String userName;
 	private String email;
+	private String phoneNum;
 	private String msg;
 }
