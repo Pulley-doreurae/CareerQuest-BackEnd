f677cbda629b2ae64981a145ec99e1a9456eb73a
pjh5365 <pjh5365@naver.com>
2024-01-17 21:42:31 +0900

    Remove: 패키지 경로 변경
    시큐리티 설정 패키지 위치를 이동하였다.
    
    Related to: #5

--- src/main/java/pulleydoreurae/chwijunjindan/auth/config/SecurityConfig.java
+++ src/main/java/pulleydoreurae/chwijunjindan/auth/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package pulleydoreurae.chwijunjindan.auth;
+package pulleydoreurae.chwijunjindan.auth.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
