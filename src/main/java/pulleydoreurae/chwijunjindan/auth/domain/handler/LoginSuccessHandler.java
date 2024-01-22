ae9a230963a768ed1bfb0156bbfdd1bbc5d2243c
pjh5365 <pjh5365@naver.com>
2024-01-22 17:49:07 +0900

    Feat: 로그인에 성공하면 Redis 를 사용하도록 추가
    로그인에 성공하면 액세스 토큰을 Redis 에 저장하도록 추가함
    
    Related to: #5

--- src/main/java/pulleydoreurae/chwijunjindan/auth/domain/handler/LoginSuccessHandler.java
+++ src/main/java/pulleydoreurae/chwijunjindan/auth/domain/handler/LoginSuccessHandler.java
@@ -13,9 +13,11 @@ import com.google.gson.Gson;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import pulleydoreurae.chwijunjindan.auth.domain.jwt.entity.JwtAccessToken;
 import pulleydoreurae.chwijunjindan.auth.domain.jwt.entity.JwtRefreshToken;
 import pulleydoreurae.chwijunjindan.auth.domain.jwt.JwtTokenProvider;
 import pulleydoreurae.chwijunjindan.auth.domain.jwt.dto.JwtTokenResponse;
+import pulleydoreurae.chwijunjindan.auth.domain.jwt.repository.JwtAccessTokenRepository;
 import pulleydoreurae.chwijunjindan.auth.domain.jwt.repository.JwtRefreshTokenRepository;
 
 /**
@@ -28,13 +30,16 @@ import pulleydoreurae.chwijunjindan.auth.domain.jwt.repository.JwtRefreshTokenRe
 public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
 	private final JwtRefreshTokenRepository jwtRefreshTokenRepository;
+	private final JwtAccessTokenRepository jwtAccessTokenRepository;
 	private final JwtTokenProvider jwtTokenProvider;
 	private final Gson gson;
 
 	@Autowired
-	public LoginSuccessHandler(JwtRefreshTokenRepository jwtRefreshTokenRepository, JwtTokenProvider jwtTokenProvider,
+	public LoginSuccessHandler(JwtRefreshTokenRepository jwtRefreshTokenRepository,
+			JwtAccessTokenRepository jwtAccessTokenRepository, JwtTokenProvider jwtTokenProvider,
 			Gson gson) {
 		this.jwtRefreshTokenRepository = jwtRefreshTokenRepository;
+		this.jwtAccessTokenRepository = jwtAccessTokenRepository;
 		this.jwtTokenProvider = jwtTokenProvider;
 		this.gson = gson;
 	}
@@ -45,6 +50,10 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
 		// 로그인에 성공하면 토큰들을 dto 로 생성
 		JwtTokenResponse jwtTokenResponse = jwtTokenProvider.createJwtResponse(authentication.getName());
+
+		// 로그인한 id 와 생성한 액세스토큰을 redis 에 저장
+		jwtAccessTokenRepository
+				.save(new JwtAccessToken(jwtTokenResponse.getAccess_token(), authentication.getName()));
 
 		// 로그인한 id 와 생성된 리프레시토큰 을 redis 에 저장
 		jwtRefreshTokenRepository
