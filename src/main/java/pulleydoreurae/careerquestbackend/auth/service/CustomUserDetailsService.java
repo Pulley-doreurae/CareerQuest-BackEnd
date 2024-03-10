ae045259701df4c7d403613a37f44007d8a69ee2
pjh5365 <pjh5365@naver.com>
2024-03-10 16:00:34 +0900

    Feat: email 로도 로그인 가능하도록 추가
    사용자 아이디 뿐만 아니라 이메일로 로그인 요청을 보내도 로그인 하도록 코드를 추가함
    
    Related to: #16

--- src/main/java/pulleydoreurae/careerquestbackend/auth/service/CustomUserDetailsService.java
+++ src/main/java/pulleydoreurae/careerquestbackend/auth/service/CustomUserDetailsService.java
@@ -1,5 +1,7 @@
 package pulleydoreurae.careerquestbackend.auth.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -28,17 +30,21 @@ public class CustomUserDetailsService implements UserDetailsService {
 	/**
 	 * 사용자 로그인 처리 메서드
 	 *
-	 * @param username 사용자의 id 를 입력받는다.
+	 * @param username 사용자의 id 를 입력받는다. (email 이 전달되는 경우도 추가하여 id, email 두 경우 모두 확인한다.)
 	 * @throws UsernameNotFoundException 사용자 정보를 찾지 못하는 경우 해당 에러를 던진다.
 	 * @return 사용자 정보를 찾을 수 있는 경우 CustomUserDetails 에 사용자 정보를 넘겨 새로운 사용자 정보를 만들어 리턴한다.
 	 */
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-		UserAccount user = userAccountRepository.findByUserId(username)
-				.orElseThrow(() -> {
-					return new UsernameNotFoundException("해당 사용자 정보를 찾을 수 없습니다.");
-				});
-		return new CustomUserDetails(user);
+		Optional<UserAccount> user = userAccountRepository.findByUserId(username);
+		if (user.isEmpty()) { // userId 로 검색하지 못했다면 email 로 검색
+			user = userAccountRepository.findByEmail(username);
+		}
+		// email 로 검색해도 정보를 찾지 못했다면
+		if (user.isEmpty()) {
+			throw new UsernameNotFoundException("해당 사용자 정보를 찾을 수 없습니다.");
+		}
+		return new CustomUserDetails(user.get());
 	}
 }
