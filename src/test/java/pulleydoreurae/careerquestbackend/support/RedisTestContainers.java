9bd3b6430d103c00acef2639ee4558b401a1cc1a
pjh5365 <pjh5365@naver.com>
2024-03-18 17:05:24 +0900

    Test: Testcontainer 수정
    Testcontainer 호출 방식 수정
    
    Related to: #18

--- src/test/java/pulleydoreurae/careerquestbackend/support/RedisTestContainers.java
+++ src/test/java/pulleydoreurae/careerquestbackend/support/RedisTestContainers.java
@@ -1,8 +1,8 @@
 package pulleydoreurae.careerquestbackend.support;
 
-import org.springframework.context.annotation.Configuration;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 /**
@@ -11,22 +11,12 @@ import org.testcontainers.utility.DockerImageName;
  * @author : parkjihyeok
  * @since : 2024/03/14
  */
-@Configuration
-@Testcontainers
-public class RedisTestContainers {
-
-	private static final String REDIS_DOCKER_IMAGE = "redis:6-alpine";
-
-	static {    // 위의 이미지로 컨테이너 생성
-		GenericContainer<?> REDIS_CONTAINER =
-				new GenericContainer<>(DockerImageName.parse(REDIS_DOCKER_IMAGE))
-						.withExposedPorts(6379) // 컨테이너의 6379 포트 오픈
-						.withReuse(true); // 테스트 사이에서 컨테이너 재사용하기
-
-		REDIS_CONTAINER.start();    // 레디스 컨테이너 실행
-
-		// 컨테이너에 올라간 레디스와 매핑시켜준다.
-		System.setProperty("spring.redis.host", REDIS_CONTAINER.getHost());
-		System.setProperty("spring.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
+public class RedisTestContainers implements BeforeAllCallback {
+	public void beforeAll(ExtensionContext context) throws Exception {
+		GenericContainer redis = new GenericContainer(DockerImageName.parse("redis:6-alpine"))
+				.withExposedPorts(6379);
+		redis.start();
+		System.setProperty("spring.data.redis.host", redis.getHost());
+		System.setProperty("spring.data.redis.port", String.valueOf(redis.getMappedPort(6379)));
 	}
 }
