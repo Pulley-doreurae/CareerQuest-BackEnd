d0cdd43c06a7fbb91543c0489c5258bbb57fbeff
pjh5365 <pjh5365@naver.com>
2024-03-26 21:42:11 +0900

    Feat: Repository 추가
    직무 및 기술스택 Repository 추가
    
    Related to: #26

---
+++ src/main/java/pulleydoreurae/careerquestbackend/auth/repository/UserCareerDetailsRepository.java
@@ -0,0 +1,14 @@
+package pulleydoreurae.careerquestbackend.auth.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import pulleydoreurae.careerquestbackend.auth.domain.entity.UserCareerDetails;
+
+/**
+ * 사용자 직무 Repository
+ *
+ * @author : parkjihyeok
+ * @since : 2024/03/26
+ */
+public interface UserCareerDetailsRepository extends JpaRepository<UserCareerDetails, Long> {
+}
