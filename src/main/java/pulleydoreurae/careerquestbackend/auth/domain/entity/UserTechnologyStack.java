fb9b1db27cb02e50c2d62518fe77ac13e80d9b04
pjh5365 <pjh5365@naver.com>
2024-03-26 21:39:50 +0900

    Feat: 엔티티 추가 및 연관관계 매핑
    - 직무 및 기술스택 엔티티 추가
    - 기존의 회원정보와 연관관계 매핑
    
    Related to: #26

---
+++ src/main/java/pulleydoreurae/careerquestbackend/auth/domain/entity/UserTechnologyStack.java
@@ -0,0 +1,37 @@
+package pulleydoreurae.careerquestbackend.auth.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 기술스택 엔티티
+ *
+ * @author : parkjihyeok
+ * @since : 2024/03/26
+ */
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserTechnologyStack extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id", nullable = false)
+	private UserAccount userAccount; // 회원정보
+	@Column(nullable = false)
+	private Long stackId; // 기술스택 id
+}
