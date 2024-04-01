3abc6d2b1ca7cd68b7896416d0fac2d1129fade7
pjh5365 <pjh5365@naver.com>
2024-04-01 16:22:24 +0900

    Feat: 게시글에 댓글 연관관계 매핑 추가
    게시글 엔티티에 댓글 엔티티와 연관관계 매핑을 추가함
    
    Related to: #31

--- src/main/java/pulleydoreurae/careerquestbackend/community/domain/entity/Post.java
+++ src/main/java/pulleydoreurae/careerquestbackend/community/domain/entity/Post.java
@@ -1,5 +1,7 @@
 package pulleydoreurae.careerquestbackend.community.domain.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -52,4 +55,7 @@ public class Post extends BaseEntity {
 
 	@Column(nullable = false)
 	private Long category; // 카테고리
+
+	@OneToMany(mappedBy = "post")
+	private List<Comment> comments; // 댓글 리스트
 }
