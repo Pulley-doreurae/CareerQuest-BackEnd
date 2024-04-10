6a0bbe4d35d7973b6a8ec507d47c82e6c6b4a725
pjh5365 <pjh5365@naver.com>
2024-04-10 19:30:41 +0900

    Feat: 게시글 서비스 로직에 사진 기능 추가
    게시글 작성 시 사진도 함께 저장할 수 있도록 추가함
    
    Related to: #31

--- src/main/java/pulleydoreurae/careerquestbackend/community/service/PostService.java
+++ src/main/java/pulleydoreurae/careerquestbackend/community/service/PostService.java
@@ -1,28 +1,35 @@
 package pulleydoreurae.careerquestbackend.community.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import pulleydoreurae.careerquestbackend.auth.domain.entity.UserAccount;
 import pulleydoreurae.careerquestbackend.auth.repository.UserAccountRepository;
+import pulleydoreurae.careerquestbackend.common.service.FileManagementService;
 import pulleydoreurae.careerquestbackend.community.domain.dto.request.PostRequest;
 import pulleydoreurae.careerquestbackend.community.domain.dto.response.PostResponse;
 import pulleydoreurae.careerquestbackend.community.domain.entity.Post;
+import pulleydoreurae.careerquestbackend.community.domain.entity.PostImage;
 import pulleydoreurae.careerquestbackend.community.domain.entity.PostViewCheck;
 import pulleydoreurae.careerquestbackend.community.repository.CommentRepository;
+import pulleydoreurae.careerquestbackend.community.repository.PostImageRepository;
 import pulleydoreurae.careerquestbackend.community.repository.PostLikeRepository;
 import pulleydoreurae.careerquestbackend.community.repository.PostRepository;
 import pulleydoreurae.careerquestbackend.community.repository.PostViewCheckRepository;
@@ -42,16 +49,24 @@ public class PostService {
 	private final CommentRepository commentRepository;
 	private final PostLikeRepository postLikeRepository;
 	private final PostViewCheckRepository postViewCheckRepository;
+	private final PostImageRepository postImageRepository;
+	private final FileManagementService fileManagementService;
+
+	@Value("${IMAGES_PATH}")
+	private String IMAGE_PATH;
 
 	@Autowired
 	public PostService(PostRepository postRepository, UserAccountRepository userAccountRepository,
 			CommentRepository commentRepository, PostLikeRepository postLikeRepository,
-			PostViewCheckRepository postViewCheckRepository) {
+			PostViewCheckRepository postViewCheckRepository, PostImageRepository postImageRepository,
+			FileManagementService fileManagementService) {
 		this.postRepository = postRepository;
 		this.userAccountRepository = userAccountRepository;
 		this.commentRepository = commentRepository;
 		this.postLikeRepository = postLikeRepository;
 		this.postViewCheckRepository = postViewCheckRepository;
+		this.postImageRepository = postImageRepository;
+		this.fileManagementService = fileManagementService;
 	}
 
 	/**
@@ -149,6 +164,7 @@ public class PostService {
 				.userId(post.getUserAccount().getUserId())
 				.title(post.getTitle())
 				.content(post.getContent())
+				.images(postImageToStringList(post))
 				.hit(post.getHit())
 				.commentCount(countComment(post.getId()))
 				.postLikeCount(countPostLike(post.getId()))
@@ -160,19 +176,67 @@ public class PostService {
 	}
 
 	/**
+	 * 이미지 저장 메서드
+	 *
+	 * @param images 컨트롤러로부터 전달받은 이미지 파일들
+	 * @return 한장이라도 실패하면 null, 성공하면 저장한 이미지 파일들의 파일명을 리스트 형태로 반환
+	 */
+	public List<String> saveImage(List<MultipartFile> images) {
+		List<String> successFiles = new ArrayList<>();
+		images.forEach(image -> {
+			String savedFile = fileManagementService.saveFile(image, IMAGE_PATH);
+			// 파일 저장에 한건이라도 실패한다면
+			if (savedFile == null) {
+				fileManagementService.deleteFile(successFiles, IMAGE_PATH);
+				throw new RuntimeException("파일 저장에 실패했습니다.");
+			}
+			successFiles.add(savedFile);
+		});
+		return successFiles;
+	}
+
+	/**
 	 * 게시글 저장 메서드
 	 *
 	 * @param postRequest 게시글 요청
 	 * @return 게시글 저장에 성공하면 true 실패하면 false 리턴
 	 */
+	@Transactional // 사진 저장과 게시글 저장을 하나의 트랜잭션으로 묶음
 	public boolean savePost(PostRequest postRequest) {
 		UserAccount user = findUserAccount(postRequest.getUserId());
+		List<String> fileNames = postRequest.getImages();
+
+		// 사용자 정보를 확인할 수 없다면 (저장에 실패한다면)
 		if (user == null) {
+			// 서버에 저장된 사진이 있을경우 삭제
+			if (fileNames != null) {
+				fileManagementService.deleteFile(fileNames, IMAGE_PATH);
+			}
 			return false;
 		}
 
 		Post post = postRequestToPost(postRequest, user);
-		postRepository.save(post);
+		try {
+			postRepository.save(post);
+		} catch (Exception e) {
+			log.error("게시글 저장 실패 {}", e.getMessage());
+			// 예외가 발생해 게시글 저장에 실패한 경우 미리 저장한 사진 정보를 삭제한다.
+			if (fileNames != null) {
+				fileManagementService.deleteFile(fileNames, IMAGE_PATH);
+			}
+			throw new RuntimeException("게시글 저장에 실패했습니다.");
+		}
+
+		// 게시글 저장이 무사히 완료되고 사진이 서버에 저장되어 있다면 데이터베이스에 해당 정보 입력
+		if (fileNames != null) {
+			fileNames.forEach(fileName -> {
+				PostImage image = PostImage.builder()
+						.post(post)
+						.fileName(fileName)
+						.build();
+				postImageRepository.save(image);
+			});
+		}
 
 		return true;
 	}
@@ -192,8 +256,12 @@ public class PostService {
 			return false;
 		}
 
-		post = postRequestToPostForUpdate(post, postRequest, user);
-		postRepository.save(post);
+		if (postRequest.getImages() != null) {
+			updateImages(postRequest.getImages(), post);
+		}
+
+		Post updatedPost = postRequestToPostForUpdate(post, postRequest, user);
+		postRepository.save(updatedPost);
 
 		return true;
 	}
@@ -214,15 +282,28 @@ public class PostService {
 		}
 		postRepository.deleteById(postId);
 
+		List<PostImage> fileNames = postImageRepository.findAllByPost(post);
+		// 저장된 사진 파일이 존재한다면 게시글 삭제하면서 사진도 삭제
+		if (fileNames != null) {
+			fileManagementService.deleteFile(fileNames.stream().map(PostImage::getFileName).toList(), IMAGE_PATH);
+		}
+
 		return true;
 	}
 
+	/**
+	 * 게시글 엔티티 리스트 -> 게시글 response 리스트
+	 *
+	 * @param postList 게시글 엔티티 리스트
+	 * @return 게시글 response 리스트
+	 */
 	private List<PostResponse> postListToPostResponseList(Page<Post> postList) {
 		return postList.stream()
 				.map(post -> PostResponse.builder()
 						.userId(post.getUserAccount().getUserId())
 						.title(post.getTitle())
 						.content(post.getContent())
+						.images(postImageToStringList(post))
 						.hit(post.getHit())
 						.commentCount(countComment(post.getId()))
 						.postLikeCount(countPostLike(post.getId()))
@@ -366,5 +447,47 @@ public class PostService {
 		}
 
 		return name;
+	}
+
+	/**
+	 * 한 게시글달린 사진 파일명들을 반환하는 메서드
+	 *
+	 * @param post 게시글 정보
+	 * @return 저장된 파일명 리스트
+	 */
+	private List<String> postImageToStringList(Post post) {
+		return postImageRepository.findAllByPost(post).stream()
+				.map(PostImage::getFileName)
+				.toList();
+	}
+
+	/**
+	 * 새롭게 전달된 사진 정보를 바탕으로 삭제될 파일을 서버와 데이터베이스에서 제거하고 새로운 내용 데이터베이스에 추가하는 메서드
+	 *
+	 * @param images 업데이트된 사진정보
+	 * @param post   게시글 정보
+	 */
+	public void updateImages(List<String> images, Post post) {
+		// 서버에서 삭제할 파일명 찾기
+		List<String> fileNamesToDelete = postImageRepository.findAllByPost(post).stream()
+				.map(PostImage::getFileName)
+				.filter(fileName -> !images.contains(fileName)).toList();
+
+		// 서버에서 먼저 파일들 삭제
+		fileManagementService.deleteFile(fileNamesToDelete, IMAGE_PATH);
+		// 삭제한 파일들 데이터베이스에서 삭제
+		fileNamesToDelete.forEach(postImageRepository::deleteByFileName);
+
+		// 새로 추가된 파일명들만 데이터베이스에 저장
+		images.stream()
+				.filter(fileName -> !postImageRepository.existsByFileName(fileName))
+				.forEach(fileName -> {
+					PostImage image = PostImage.builder()
+							.post(post)
+							.fileName(fileName)
+							.build();
+
+					postImageRepository.save(image);
+				});
 	}
 }
