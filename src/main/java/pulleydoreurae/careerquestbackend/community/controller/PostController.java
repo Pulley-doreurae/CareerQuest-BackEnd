044b85d6299483fe45c7241e6cea9684c49877e0
pjh5365 <pjh5365@naver.com>
2024-05-24 16:26:57 +0900

    Refactor: 커뮤니티 추상화 전으로 롤백 - Controller
    Related to: #47

--- src/main/java/pulleydoreurae/careerquestbackend/community/controller/PostController.java
+++ src/main/java/pulleydoreurae/careerquestbackend/community/controller/PostController.java
@@ -3,13 +3,14 @@ package pulleydoreurae.careerquestbackend.community.controller;
 import java.net.MalformedURLException;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -24,12 +25,13 @@ import org.springframework.web.multipart.MultipartFile;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
-import pulleydoreurae.careerquestbackend.common.community.controller.PostController;
+import lombok.RequiredArgsConstructor;
+import pulleydoreurae.careerquestbackend.common.dto.response.SimpleResponse;
 import pulleydoreurae.careerquestbackend.community.domain.PostCategory;
 import pulleydoreurae.careerquestbackend.community.domain.dto.request.PostRequest;
+import pulleydoreurae.careerquestbackend.community.domain.dto.response.PostFailResponse;
 import pulleydoreurae.careerquestbackend.community.domain.dto.response.PostResponse;
-import pulleydoreurae.careerquestbackend.common.community.service.PostService;
-import pulleydoreurae.careerquestbackend.common.dto.response.SimpleResponse;
+import pulleydoreurae.careerquestbackend.community.service.PostService;
 
 /**
  * 게시글을 담당하는 Controller
@@ -37,74 +39,229 @@ import pulleydoreurae.careerquestbackend.common.dto.response.SimpleResponse;
  * @author : parkjihyeok
  * @since : 2024/03/28
  */
-@RestController
 @RequestMapping("/api")
-public class BasicPostController extends PostController {
+@RestController
+@RequiredArgsConstructor
+public class PostController {
 
-	public BasicPostController(@Qualifier("basicPostService") PostService postService) {
-		super(postService);
-	}
+	private final PostService postService;
 
+	/**
+	 * 게시글 전체 조회
+	 *
+	 * @param pageable 페이지 정보
+	 * @return 게시글 리스트
+	 */
 	@GetMapping("/posts")
 	public ResponseEntity<List<PostResponse>> getPostList(@PageableDefault(size = 15) Pageable pageable) {
-		return super.getPostList(pageable);
+		List<PostResponse> posts = postService.getPostResponseList(pageable);
+
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(posts);
 	}
 
+	/**
+	 * 카테고리별 게시글 조회
+	 *
+	 * @param postCategory 카테고리 정보
+	 * @param pageable     페이지 정보
+	 * @return 게시글 리스트
+	 */
 	@GetMapping("/posts/category/{postCategory}")
 	public ResponseEntity<List<PostResponse>> getPostListByCategory(@PathVariable PostCategory postCategory,
 			@PageableDefault(size = 15) Pageable pageable) {
 
-		return super.getPostListByCategory(postCategory, pageable);
+		List<PostResponse> posts = postService.getPostResponseListByCategory(postCategory, pageable);
+
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(posts);
 	}
 
+	/**
+	 * 작성자별 게시글 조회
+	 *
+	 * @param userId   작성자
+	 * @param pageable 페이지 정보
+	 * @return 게시글 리스트
+	 */
 	@GetMapping("/posts/user/{userId}")
 	public ResponseEntity<?> getPostListByUserId(@PathVariable String userId,
 			@PageableDefault(size = 15) Pageable pageable) {
 
-		return super.getPostListByUserId(userId, pageable);
+		List<PostResponse> posts = postService.getPostListByUserAccount(userId, pageable);
+
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(posts);
 	}
 
+	/**
+	 * 검색한 게시글 조회
+	 *
+	 * @param keyword      검색어
+	 * @param postCategory 카테고리 정보
+	 * @param pageable     페이지 정보
+	 * @return 게시글 리스트
+	 */
 	@GetMapping("/posts/search")
 	public ResponseEntity<List<PostResponse>> searchPosts(@RequestParam(name = "keyword") String keyword,
 			@RequestParam(name = "postCategory", required = false) PostCategory postCategory,
 			@PageableDefault(size = 15, direction = Sort.Direction.DESC) Pageable pageable) {
 
-		return super.searchPosts(keyword, postCategory, pageable);
+		List<PostResponse> posts = postService.searchPosts(keyword, postCategory, pageable);
+
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(posts);
 	}
 
+	/**
+	 * 게시글 단건 조회
+	 *
+	 * @param request  요청자 정보
+	 * @param response 응답
+	 * @param postId   게시글 정보
+	 * @return 게시글
+	 */
 	@GetMapping("/posts/{postId}")
 	public ResponseEntity<?> getPost(HttpServletRequest request, HttpServletResponse response,
 			@PathVariable Long postId) {
 
-		return super.getPost(request, response, postId);
+		PostResponse post = postService.findByPostId(request, response, postId);
+
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(post);
 	}
 
+	/**
+	 * 사진 조회
+	 *
+	 * @param fileName 사진 정보
+	 * @return 사진 경로
+	 * @throws MalformedURLException 사진 정보를 찾지 못했을 경우
+	 */
 	@GetMapping("/posts/images/{fileName}")
 	public Resource getImage(@PathVariable String fileName) throws MalformedURLException {
-		return super.getImage(fileName);
+		return postService.getImageResource(fileName);
 	}
 
+	/**
+	 * 사진 저장
+	 *
+	 * @param images 이미지 정보
+	 * @return 저장된 사진 정보 리스트
+	 */
 	@PostMapping("/posts/images")
 	public ResponseEntity<List<String>> saveImage(List<MultipartFile> images) {
-		return super.saveImage(images);
+		List<String> imagesName = postService.saveImage(images);
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(imagesName);
 	}
 
+	/**
+	 * 게시글 저장
+	 *
+	 * @param postRequest   게시글 정보
+	 * @param bindingResult 에러 검증
+	 * @return 처리에 대한 결과
+	 */
 	@PostMapping("/posts")
 	public ResponseEntity<?> savePost(@Valid @RequestBody PostRequest postRequest,
 			BindingResult bindingResult) {
 
-		return super.savePost(postRequest, bindingResult);
+		// 검증
+		ResponseEntity<PostFailResponse> BAD_REQUEST = validCheck(postRequest, bindingResult);
+		if (BAD_REQUEST != null) {
+			return BAD_REQUEST;
+		}
+
+		postService.savePost(postRequest);
+
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(SimpleResponse.builder()
+						.msg("게시글 등록에 성공했습니다.")
+						.build());
 	}
 
+	/**
+	 * 게시글 수정
+	 *
+	 * @param postId        게시글 정보
+	 * @param postRequest   업데이트 정보
+	 * @param bindingResult 에러 검증
+	 * @return 처리 결과
+	 */
 	@PatchMapping("/posts/{postId}")
 	public ResponseEntity<?> updatePost(@PathVariable Long postId,
 			@Valid @RequestBody PostRequest postRequest, BindingResult bindingResult) {
 
-		return super.updatePost(postId, postRequest, bindingResult);
+		// 검증
+		ResponseEntity<PostFailResponse> BAD_REQUEST = validCheck(postRequest, bindingResult);
+		if (BAD_REQUEST != null) {
+			return BAD_REQUEST;
+		}
+
+		if (!postService.updatePost(postId, postRequest)) {
+			return makeBadRequestUsingSimpleResponse("해당 게시글을 수정할 수 없습니다.");
+		}
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(SimpleResponse.builder()
+						.msg("게시글 수정에 성공했습니다.")
+						.build());
 	}
 
+	/**
+	 * 게시글 삭제
+	 *
+	 * @param postId 게시글 정보
+	 * @param userId 요청자 정보
+	 * @return 처리 결과
+	 */
 	@DeleteMapping("/posts/{postId}")
 	public ResponseEntity<SimpleResponse> deletePost(@PathVariable Long postId, String userId) {
-		return super.deletePost(postId, userId);
+		if (!postService.deletePost(postId, userId)) {
+			return makeBadRequestUsingSimpleResponse("해당 게시글을 삭제할 수 없습니다.");
+		}
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(SimpleResponse.builder()
+						.msg("게시글 삭제에 성공하였습니다.")
+						.build());
+	}
+
+	/**
+	 * SimpleResponse 형태의 BAD_REQUEST 생성 메서드
+	 *
+	 * @param message 작성할 메시지
+	 * @return 완성된 BAD_REQUEST
+	 */
+	private ResponseEntity<SimpleResponse> makeBadRequestUsingSimpleResponse(String message) {
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+				.body(SimpleResponse.builder()
+						.msg(message)
+						.build());
+	}
+
+	/**
+	 * 검증 메서드
+	 *
+	 * @param postRequest   게시글 요청 (검증에 실패하더라도 입력한 값은 그대로 돌려준다.)
+	 * @param bindingResult 검증 결과
+	 * @return 검증결과 에러가 없다면 null 에러가 있다면 해당 에러를 담은 ResponseEntity 반환
+	 */
+	private ResponseEntity<PostFailResponse> validCheck(PostRequest postRequest, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			String[] errors = new String[bindingResult.getAllErrors().size()];
+			int index = 0;
+			for (ObjectError error : bindingResult.getAllErrors()) {
+				errors[index++] = error.getDefaultMessage();
+			}
+
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(PostFailResponse.builder()
+							.title(postRequest.getTitle())
+							.content(postRequest.getContent())
+							.postCategory(postRequest.getPostCategory())
+							.errors(errors)
+							.build());
+		}
+		return null;
 	}
 }
--- src/main/java/pulleydoreurae/careerquestbackend/community/controller/PostLikeController.java
+++ src/main/java/pulleydoreurae/careerquestbackend/community/controller/PostLikeController.java
@@ -2,9 +2,9 @@ package pulleydoreurae.careerquestbackend.community.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
-import pulleydoreurae.careerquestbackend.common.community.controller.PostLikeController;
+import lombok.RequiredArgsConstructor;
+import pulleydoreurae.careerquestbackend.common.dto.response.SimpleResponse;
 import pulleydoreurae.careerquestbackend.community.domain.dto.request.PostLikeRequest;
 import pulleydoreurae.careerquestbackend.community.domain.dto.response.PostResponse;
-import pulleydoreurae.careerquestbackend.common.community.service.PostLikeService;
-import pulleydoreurae.careerquestbackend.common.dto.response.SimpleResponse;
+import pulleydoreurae.careerquestbackend.community.service.PostLikeService;
 
 /**
  * 좋아요 Controller
@@ -27,27 +27,73 @@ import pulleydoreurae.careerquestbackend.common.dto.response.SimpleResponse;
  * @author : parkjihyeok
  * @since : 2024/04/03
  */
-@RestController
 @RequestMapping("/api")
-public class BasicPostLikeController extends PostLikeController {
+@RestController
+@RequiredArgsConstructor
+public class PostLikeController {
 
-	public BasicPostLikeController(@Qualifier("basicPostLikeService") PostLikeService postLikeService) {
-		super(postLikeService);
-	}
+	private final PostLikeService postLikeService;
 
+	/**
+	 * 좋아요 상태를 변경하는 메서드
+	 *
+	 * @param postLikeRequest 좋아요 요청
+	 * @param bindingResult   에러검증
+	 * @return 처리결과
+	 */
 	@PostMapping("/posts/likes")
-	@Override
 	public ResponseEntity<SimpleResponse> changeLikeStatus(@Valid @RequestBody PostLikeRequest postLikeRequest,
 			BindingResult bindingResult) {
 
-		return super.changeLikeStatus(postLikeRequest, bindingResult);
+		// 검증
+		ResponseEntity<SimpleResponse> BAD_REQUEST = validCheck(bindingResult);
+		if (BAD_REQUEST != null) {
+			return BAD_REQUEST;
+		}
+
+		postLikeService.changePostLike(postLikeRequest);
+
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(SimpleResponse.builder()
+						.msg("좋아요 상태 변경에 성공했습니다.")
+						.build());
 	}
 
+	/**
+	 * 한 사용자가 좋아요 누른 게시글 리스트 반환 메서드
+	 *
+	 * @param userId   사용자 id
+	 * @param pageable 페이지
+	 * @return 조회한 리스트
+	 */
 	@GetMapping("/posts/likes/{userId}")
-	@Override
 	public ResponseEntity<List<PostResponse>> findAllPostLikeByUserAccount(@PathVariable String userId,
 			@PageableDefault(size = 15) Pageable pageable) {
 
-		return super.findAllPostLikeByUserAccount(userId, pageable);
+		List<PostResponse> result = postLikeService.findAllPostLikeByUserAccount(userId, pageable);
+
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(result);
+	}
+
+	/**
+	 * 검증 메서드
+	 *
+	 * @param bindingResult 검증 결과
+	 * @return 검증결과 에러가 없다면 null 에러가 있다면 해당 에러를 담은 ResponseEntity 반환
+	 */
+	private ResponseEntity<SimpleResponse> validCheck(BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			StringBuilder sb = new StringBuilder();
+			bindingResult.getAllErrors().forEach(error -> {
+				sb.append(error.getDefaultMessage()).append("\n");
+			});
+
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+					.body(SimpleResponse.builder()
+							.msg(sb.toString())
+							.build());
+		}
+		return null;
 	}
 }
