153c58c4401917acda7f11801da59700f27b1fe8
pjh5365 <pjh5365@naver.com>
2024-05-24 14:27:05 +0900

    Refactor: 커뮤니티 추상화 전으로 롤백하기 - Entity
    Related to: #47

---
+++ src/main/java/pulleydoreurae/careerquestbackend/community/domain/entity/PostImage.java
@@ -0,0 +1,39 @@
+package pulleydoreurae.careerquestbackend.community.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import pulleydoreurae.careerquestbackend.common.entity.BaseEntity;
+
+/**
+ * 게시글에 사용되는 이미지 Entity
+ *
+ * @author : parkjihyeok
+ * @since : 2024/04/08
+ */
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PostImage extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String fileName;
+
+	@ManyToOne
+	@JoinColumn(nullable = false, name = "post_id")
+	private Post post;
+}
