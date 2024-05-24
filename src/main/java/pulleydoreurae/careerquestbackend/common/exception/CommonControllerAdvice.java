43be95d0b498a63498a64a12af17d0b3c8fc6e2c
pjh5365 <pjh5365@naver.com>
2024-05-24 22:48:40 +0900

    Refactor: 클래스 이동에 따른 import 수정
    Related to: #47

--- src/main/java/pulleydoreurae/careerquestbackend/common/exception/CommonControllerAdvice.java
+++ src/main/java/pulleydoreurae/careerquestbackend/common/exception/CommonControllerAdvice.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import lombok.extern.slf4j.Slf4j;
 import pulleydoreurae.careerquestbackend.common.dto.response.SimpleResponse;
-import pulleydoreurae.careerquestbackend.common.community.exception.CommunityException;
+import pulleydoreurae.careerquestbackend.community.exception.CommunityException;
 
 /**
  * 컨트롤러 예외처리
@@ -24,7 +24,7 @@ public class CommonControllerAdvice {
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<SimpleResponse> common(Exception e) {
-		log.error(" : {}", e.getMessage());
+		log.error("예상치 못한 오류 : {}", e.getMessage());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
 				.body(SimpleResponse.builder().msg("예상치 못한 오류가 발생했습니다. 관리자에게 문의해주세요.").build());
 	}
