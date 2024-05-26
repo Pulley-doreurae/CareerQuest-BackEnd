5e73aa47219395ef4b6056bf8681535484630732
pjh5365 <pjh5365@naver.com>
2024-05-26 10:03:52 +0900

    Feat: 공모전 검색을 위한 Request클래스와 공모전의 정보를 저장할 Entity클래스 추가
    Related to: #47

---
+++ src/main/java/pulleydoreurae/careerquestbackend/community/domain/dto/request/ContestSearchRequest.java
@@ -0,0 +1,27 @@
+package pulleydoreurae.careerquestbackend.community.domain.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 공모전 검색조건 Request
+ *
+ * @author : parkjihyeok
+ * @since : 2024/05/25
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ContestSearchRequest {
+
+	private String contestCategory; // 공모전 분야 -> enum으로 변경 가능할듯?
+	private String target; // 대상 -> enum으로 변경 가능할듯?
+	private String region; // 개최지역
+	private String organizer; // 주관처
+	private Long totalPrize; // 총상금
+}
