d24e22dee09a231e0304d3a20bdda13c75b1d158
pjh5365 <pjh5365@naver.com>
2024-05-30 21:45:08 +0900

    Feat: dto클래스 검증 추가
    Related to: #57

--- src/main/java/pulleydoreurae/careerquestbackend/certification/domain/dto/request/UserCertificationRequest.java
+++ src/main/java/pulleydoreurae/careerquestbackend/certification/domain/dto/request/UserCertificationRequest.java
@@ -2,6 +2,8 @@ package pulleydoreurae.careerquestbackend.certification.domain.dto.request;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,7 +19,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UserCertificationRequest {
 
+	@NotBlank(message = "회원ID는 공백일 수 없습니다.")
 	private String userId;
+	@NotBlank(message = "자격증 이름은 공백일 수 없습니다.")
 	private String certificationName;
+	@NotNull(message = "취득일자는 null일 수 없습니다.")
 	private LocalDate acqDate;
 }
