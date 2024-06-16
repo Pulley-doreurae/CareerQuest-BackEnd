ab828b2b5b312dbbab5ce8b1f3d4997002a1adc6
pjh5365 <pjh5365@naver.com>
2024-06-16 18:34:36 +0900

    Feat: 공모전 참여에 대한 서비스 로직 추가
    Related to: #83

--- src/main/java/pulleydoreurae/careerquestbackend/community/service/ContestService.java
+++ src/main/java/pulleydoreurae/careerquestbackend/community/service/ContestService.java
@@ -10,15 +10,21 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import pulleydoreurae.careerquestbackend.auth.domain.entity.UserAccount;
+import pulleydoreurae.careerquestbackend.common.service.CommonService;
 import pulleydoreurae.careerquestbackend.community.domain.dto.request.ContestRequest;
 import pulleydoreurae.careerquestbackend.community.domain.dto.request.ContestSearchRequest;
+import pulleydoreurae.careerquestbackend.community.domain.dto.request.JoinContestRequest;
 import pulleydoreurae.careerquestbackend.community.domain.dto.request.PostRequest;
 import pulleydoreurae.careerquestbackend.community.domain.dto.response.ContestResponse;
+import pulleydoreurae.careerquestbackend.community.domain.dto.response.JoinContestResponse;
 import pulleydoreurae.careerquestbackend.community.domain.entity.Contest;
+import pulleydoreurae.careerquestbackend.community.domain.entity.JoinedContest;
 import pulleydoreurae.careerquestbackend.community.domain.entity.Post;
 import pulleydoreurae.careerquestbackend.community.exception.PostNotFoundException;
 import pulleydoreurae.careerquestbackend.community.exception.PostSaveException;
 import pulleydoreurae.careerquestbackend.community.repository.ContestRepository;
+import pulleydoreurae.careerquestbackend.community.repository.JoinedContestRepository;
 import pulleydoreurae.careerquestbackend.community.repository.PostRepository;
 
 /**
@@ -35,6 +41,8 @@ public class ContestService {
 	private final PostRepository postRepository;
 	private final ContestRepository contestRepository;
 	private final CommonCommunityService commonCommunityService;
+	private final JoinedContestRepository joinedContestRepository;
+	private final CommonService commonService;
 
 	/**
 	 * 게시글 + 공모전정보를 함께 저장하는 메서드 (하나의 트랜잭션으로 묶어 게시글 저장에 실패하면 공모전 저장에도 실패한다.)
@@ -163,5 +171,61 @@ public class ContestService {
 		});
 
 		return response;
+	}
+
+	/**
+	 * 공모전 참여 메서드
+	 *
+	 * @param request 참여 요청
+	 */
+	public void joinContest(JoinContestRequest request) {
+		Contest contest = findContest(request.getContestId());
+		UserAccount user = commonService.findUserAccount(request.getUserId(), true);
+		JoinedContest joinedContest = JoinedContest.builder().contest(contest).userAccount(user).build();
+		joinedContestRepository.save(joinedContest);
+	}
+
+	/**
+	 * 공모전 참여에서 제거
+	 *
+	 * @param request 참여 요청
+	 */
+	public void removeFromJoinContest(JoinContestRequest request) {
+		Contest contest = findContest(request.getContestId());
+		UserAccount user = commonService.findUserAccount(request.getUserId(), true);
+		joinedContestRepository.deleteByContestIdAndUserAccount(contest.getId(), user);
+	}
+
+	/**
+	 * 참여 공모전 리스트 조회 메서드
+	 *
+	 * @param userId 회원ID
+	 * @return 결과 리스트
+	 */
+	public List<JoinContestResponse> findJoinContest(String userId) {
+		return joinedContestRepository.findByUserId(userId)
+				.stream()
+				.map(jc ->
+						JoinContestResponse.builder()
+								.postId(jc.getContest().getPost().getId())
+								.contestId(jc.getContest().getId())
+								.joinContestId(jc.getId())
+								.userId(jc.getUserAccount().getUserId())
+								.title(jc.getContest().getPost().getTitle())
+								.build()
+				).toList();
+	}
+
+	/**
+	 * 공모전을 가져오는 메서드
+	 * @param contestId 공모전 ID
+	 * @return 공모전 엔티티
+	 */
+	private Contest findContest(Long contestId) {
+		Optional<Contest> byId = contestRepository.findById(contestId);
+		if (byId.isEmpty()) {
+			throw new PostNotFoundException("공모전 정보를 찾을 수 없습니다.");
+		}
+		return byId.get();
 	}
 }
